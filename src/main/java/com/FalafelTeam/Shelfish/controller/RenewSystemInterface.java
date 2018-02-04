1b9b13633dc30ede11be4fa01851ff9a409d6513
SelkieAnna <32209793+SelkieAnna@users.noreply.github.com>
2018-02-05 02:00:46 +0300

    Delete RenewSystemInterface.java

--- src/main/java/com/FalafelTeam/Shelfish/controller/RenewSystemInterface.java
+++
@@ -1,9 +0,0 @@
-package com.FalafelTeam.Shelfish.controller;
-
-import com.FalafelTeam.Shelfish.model.Document;
-
-public interface RenewSystemInterface {
-
-    // renew document
-    void renewDocument(Document document);
-}
